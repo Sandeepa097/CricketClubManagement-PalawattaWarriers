e07372e1c129c23fb3ea0136b7718347875d77f7
Gihan Sandeepa <gihan4880@gmail.com>
2024-03-03 23:35:56 +0530

    welcome screen designed

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,11 +1,38 @@
+import React, { useEffect, useState, useCallback } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import * as SplashScreen from 'expo-splash-screen';
+import * as Font from 'expo-font';
+import BackgroundImage from './assets/BackgroundImage';
+import Welcome from './screens/Welcome';
 
 export default function App() {
+  const [appLoading, setAppLoading] = useState(true);
+
+  useEffect(() => {
+    const loadFont = async () => {
+      await Font.loadAsync({
+        'Anybody-Regular': require('./assets/fonts/Anybody-Regular.ttf'),
+      });
+      setAppLoading(false);
+    };
+
+    loadFont();
+  });
+
+  useCallback(async () => {
+    if (!appLoading) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appLoading]);
+
+  if (appLoading) return null;
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <BackgroundImage />
+      <Welcome />
     </View>
   );
 }
@@ -13,7 +40,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
---
+++ mobile/screens/Welcome.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { View, Dimensions, StyleSheet } from 'react-native';
+import LogoWithName from '../assets/LogoWithName';
+import { Colors } from '../constants/Colors';
+import Button from '../components/Button';
+
+const width: number = Dimensions.get('window').width;
+const height: number = Dimensions.get('window').height;
+
+const Welcome = () => {
+  const continueAsGuest = () => console.log('Continue as guest');
+
+  const adminPortal = () => console.log('Admin portal');
+
+  return (
+    <View style={styles.container}>
+      <LogoWithName width={0.419 * width} height={0.194 * height} />
+      <View style={styles.divider}></View>
+      <Button
+        length="short"
+        style="filled"
+        color={Colors.DEEP_TEAL}
+        text="Continue as Guest"
+        onPress={continueAsGuest}
+      />
+      <Button
+        length="short"
+        style="outlined"
+        color={Colors.DEEP_TEAL}
+        text="Admin Portal"
+        onPress={adminPortal}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'column',
+  },
+  divider: {
+    marginTop: 0.043 * height,
+    marginBottom: 0.043 * height,
+    width: 0.798 * width,
+    height: 1,
+    backgroundColor: Colors.WHITE,
+  },
+});
+
+export default Welcome;
