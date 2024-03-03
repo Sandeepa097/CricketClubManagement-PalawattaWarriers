164958b00a7cf021ee8b2d64499f167f60ba478c
Gihan Sandeepa <gihan4880@gmail.com>
2024-03-03 23:34:25 +0530

    color constants

---
+++ mobile/constants/Colors.ts
@@ -0,0 +1,12 @@
+export enum Colors {
+  DEEP_TEAL = '#004d40',
+  MEDIUM_TEAL = '#4ba593',
+  LIGHT_TEAL = '#60ae9d',
+  DARK_TEAL = '#006962',
+  DEEP_ORANGE = '#ff5722',
+  PURPLE = '#9c27b0',
+  AMBER = '#ffc107',
+  WHITE = '#ffffff',
+  OFF_WHITE = '#f0f0f0',
+  BLACK = '#000000',
+}
