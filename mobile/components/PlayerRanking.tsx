9feefc3c2dd64bc731dfec2c3c89b39478671ffe
Gihan Sandeepa <gihan4880@gmail.com>
2024-03-10 12:15:42 +0530

    leaderboard designed

---
+++ mobile/components/PlayerRanking.tsx
@@ -0,0 +1,282 @@
+import React from 'react';
+import { Dimensions, StyleSheet, Text, View, FlatList } from 'react-native';
+import Logo from '../assets/Logo';
+import { Colors } from '../constants/Colors';
+
+interface RankingProps {
+  id: string | number;
+  name: string;
+  avatar?: string;
+  points: number;
+}
+
+interface PlayerProps extends RankingProps {
+  placement: number;
+}
+
+interface TopPlayerProps extends RankingProps {
+  placement: 1 | 2 | 3;
+  color: Colors;
+}
+
+const width: number = Dimensions.get('window').width;
+const topPlayerContainerWidth = (width - 70) / 3;
+
+const TopPlayerItem = (props: TopPlayerProps) => {
+  return (
+    <View style={styles.singleTopPlayerContainer}>
+      <View style={styles.singleTopPlayerDetailsContainer}>
+        <View
+          style={[
+            styles.topPlayerShadowContainer,
+            {
+              backgroundColor: props.color + '33',
+            },
+          ]}>
+          <View
+            style={[
+              styles.topPlayerAvatarContainer,
+              {
+                borderColor: props.color,
+              },
+            ]}>
+            <Logo height={topPlayerContainerWidth - 30} fill={props.color} />
+          </View>
+        </View>
+        <View
+          style={[
+            styles.topPlayerPlacementShadowContainer,
+            {
+              backgroundColor: props.color + '33',
+            },
+          ]}>
+          <View
+            style={[
+              styles.topPlayerPlacementContainer,
+              { borderColor: props.color },
+            ]}>
+            <Text
+              style={[styles.topPlayerPlacementText, { color: props.color }]}>
+              {props.placement}
+            </Text>
+          </View>
+        </View>
+      </View>
+      <View style={[styles.singleTopPlayerDetailsContainer, { marginTop: 8 }]}>
+        <Text style={[styles.topPlayerName, { color: props.color }]}>
+          {props.name}
+        </Text>
+        <View
+          style={[
+            styles.topPlayerPointsContainer,
+            { borderColor: props.color + '33' },
+          ]}>
+          <Text style={styles.textStrokeOne}>{props.points}</Text>
+          <Text style={styles.textStrokeTwo}>{props.points}</Text>
+          <Text style={styles.textStrokeThree}>{props.points}</Text>
+          <Text style={styles.textStrokeFour}>{props.points}</Text>
+          <Text style={[styles.topPlayerPoints, { color: props.color }]}>
+            {props.points}
+          </Text>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+const PlayerItem = (props: PlayerProps) => {
+  return (
+    <View style={styles.playerContainer}>
+      <View style={styles.playerDetailsContainer}>
+        <Text style={styles.playerText}>{props.placement}</Text>
+        <View style={styles.playerAvatarContainer}>
+          <Logo height={20} fill={Colors.DEEP_TEAL} />
+        </View>
+        <Text style={styles.playerText}>{props.name}</Text>
+      </View>
+      <Text style={styles.playerText}>{props.points}</Text>
+    </View>
+  );
+};
+
+const PlayerRanking = ({ items }: { items: RankingProps[] }) => {
+  return (
+    <View style={styles.container}>
+      {items.length >= 3 && (
+        <View style={styles.topPlayersContainer}>
+          <View>
+            <TopPlayerItem
+              {...items[1]}
+              placement={2}
+              color={Colors.DEEP_ORANGE}
+            />
+          </View>
+          <View style={{ marginBottom: 40 }}>
+            <TopPlayerItem {...items[0]} placement={1} color={Colors.PURPLE} />
+          </View>
+          <View>
+            <TopPlayerItem
+              {...items[2]}
+              placement={3}
+              color={Colors.DARK_TEAL}
+            />
+          </View>
+        </View>
+      )}
+      {items.length > 3 && (
+        <FlatList
+          data={items.slice(3, items.length)}
+          renderItem={({ item, index }) => (
+            <PlayerItem key={item.id} {...item} placement={index + 4} />
+          )}
+        />
+      )}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    width: width - 20,
+    flex: 1,
+  },
+  topPlayersContainer: {
+    width: '100%',
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  topPlayerShadowContainer: {
+    width: topPlayerContainerWidth,
+    height: topPlayerContainerWidth + 6,
+    borderRadius: (topPlayerContainerWidth + 6) / 2,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  singleTopPlayerContainer: {
+    width: topPlayerContainerWidth,
+  },
+  singleTopPlayerDetailsContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  topPlayerAvatarContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: topPlayerContainerWidth,
+    height: topPlayerContainerWidth,
+    borderRadius: topPlayerContainerWidth / 2,
+    borderWidth: 3,
+    backgroundColor: Colors.OFF_WHITE,
+    marginBottom: 6,
+  },
+  topPlayerPlacementShadowContainer: {
+    position: 'absolute',
+    bottom: -8,
+    width: 24,
+    height: 24,
+    borderRadius: (topPlayerContainerWidth + 4) / 2,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  topPlayerPlacementContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 20,
+    height: 20,
+    borderRadius: 10,
+    borderWidth: 1,
+    backgroundColor: Colors.OFF_WHITE,
+    marginBottom: 4,
+  },
+  topPlayerPlacementText: {
+    fontFamily: 'Anybody-Regular',
+    fontSize: 15,
+  },
+  topPlayerName: {
+    fontFamily: 'Anybody-Regular',
+    fontSize: 15,
+    textAlign: 'center',
+  },
+  topPlayerPointsContainer: {
+    borderBottomWidth: 4,
+    borderRadius: 15,
+  },
+  topPlayerPoints: {
+    fontFamily: 'Anybody-Regular',
+    fontSize: 15,
+  },
+  textStrokeOne: {
+    marginLeft: 1,
+    fontFamily: 'Anybody-Regular',
+    fontSize: 16,
+    color: Colors.OFF_WHITE,
+    position: 'absolute',
+  },
+  textStrokeTwo: {
+    marginLeft: -1,
+    fontFamily: 'Anybody-Regular',
+    fontSize: 16,
+    color: Colors.OFF_WHITE,
+    position: 'absolute',
+  },
+  textStrokeThree: {
+    marginLeft: 1,
+    marginBottom: 1,
+    fontFamily: 'Anybody-Regular',
+    fontSize: 16,
+    color: Colors.OFF_WHITE,
+    position: 'absolute',
+  },
+  textStrokeFour: {
+    marginLeft: -1,
+    marginBottom: 1,
+    fontFamily: 'Anybody-Regular',
+    fontSize: 16,
+    color: Colors.OFF_WHITE,
+    position: 'absolute',
+  },
+  playerContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    width: width - 20,
+    borderRadius: 15,
+    backgroundColor: Colors.MEDIUM_TEAL,
+    height: 50,
+    marginBottom: 10,
+    paddingHorizontal: 15,
+  },
+  playerDetailsContainer: {
+    display: 'flex',
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+  playerAvatarContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+    backgroundColor: Colors.OFF_WHITE,
+    marginHorizontal: 10,
+  },
+  playerText: {
+    fontFamily: 'Anybody-Regular',
+    fontSize: 15,
+    color: Colors.OFF_WHITE,
+  },
+});
+
+export default PlayerRanking;
--- mobile/screens/Leaderboard.tsx
+++ mobile/screens/Leaderboard.tsx
@@ -2,9 +2,73 @@ import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import TabBar from '../components/TabBar';
 import Toggle from '../components/Toggle';
+import PlayerRanking from '../components/PlayerRanking';
 
 const Leaderboard = () => {
   const [selectedTabItem, setSelectedTabItem] = useState('overall');
+
+  const sampleOverallBoard = [
+    {
+      id: 1,
+      name: 'Adonis Ross',
+      avatar: null,
+      points: 1300,
+    },
+    {
+      id: 2,
+      name: 'Robert Robinson',
+      avatar: null,
+      points: 1200,
+    },
+    {
+      id: 3,
+      name: 'Wesley Evans',
+      avatar: null,
+      points: 1085,
+    },
+    {
+      id: 4,
+      name: 'Adonis Ross',
+      avatar: null,
+      points: 980,
+    },
+    {
+      id: 5,
+      name: 'Adonis Ross',
+      avatar: null,
+      points: 800,
+    },
+    {
+      id: 6,
+      name: 'Adonis Ross',
+      avatar: null,
+      points: 500,
+    },
+    {
+      id: 7,
+      name: 'Adonis Ross',
+      avatar: null,
+      points: 400,
+    },
+    {
+      id: 8,
+      name: 'Adonis Ross',
+      avatar: null,
+      points: 200,
+    },
+    {
+      id: 9,
+      name: 'Adonis Ross',
+      avatar: null,
+      points: 100,
+    },
+    {
+      id: 10,
+      name: 'Adonis Ross',
+      avatar: null,
+      points: 13,
+    },
+  ];
   return (
     <View style={styles.container}>
       <Toggle
@@ -22,6 +86,7 @@ const Leaderboard = () => {
         ]}
         onPressItem={(id) => setSelectedTabItem(id.toString())}
       />
+      <PlayerRanking items={sampleOverallBoard} />
     </View>
   );
 };
