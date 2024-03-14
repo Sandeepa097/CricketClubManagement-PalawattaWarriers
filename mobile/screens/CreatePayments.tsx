46cc0c5aeccf26edaf3266fd8dfbecd67e771678
Gihan Sandeepa <gihan4880@gmail.com>
2024-03-14 21:19:21 +0530

    create payment screen

--- mobile/screens/CreatePayments.tsx
+++ mobile/screens/CreatePayments.tsx
@@ -1,23 +1,48 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
 import ChildInputWithPlayers from '../components/ChildInputWithPlayers';
+import Button from '../components/base/Button';
+import { Colors } from '../constants/Colors';
 
-const CreatePayments = () => {
+const CreatePayments = ({ navigation }) => {
   const [paymentDetails, setPaymentDetails] = useState([]);
 
   return (
     <View style={styles.container}>
-      <ChildInputWithPlayers
-        placeholder="Select Payees"
-        values={paymentDetails}
-        onChangeValues={(values) => {
-          console.log(values);
-          setPaymentDetails(values);
-        }}
-        itemProperties={[
-          { type: 'text', name: 'amount', placeholder: 'Paid Amount' },
-        ]}
-      />
+      <ScrollView
+        contentContainerStyle={{
+          display: 'flex',
+          alignItems: 'center',
+          marginTop: 20,
+        }}>
+        <ChildInputWithPlayers
+          placeholder="Select Payers"
+          values={paymentDetails}
+          onChangeValues={(values) => {
+            console.log(values);
+            setPaymentDetails(values);
+          }}
+          itemProperties={[
+            { type: 'text', name: 'amount', placeholder: 'Paid Amount' },
+          ]}
+        />
+        <View style={{ marginTop: 40 }}>
+          <Button
+            length="long"
+            style="filled"
+            color={Colors.DEEP_TEAL}
+            text="Create"
+            onPress={() => console.log('create')}
+          />
+          <Button
+            length="long"
+            style="outlined"
+            color={Colors.DEEP_TEAL}
+            text="Cancel"
+            onPress={() => navigation.goBack()}
+          />
+        </View>
+      </ScrollView>
     </View>
   );
 };
