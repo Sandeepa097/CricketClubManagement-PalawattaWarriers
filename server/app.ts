76b0be78970f5d66fdb69646770fca258727a74f
Gihan Sandeepa <gihan4880@gmail.com>
2024-03-24 11:20:50 +0530

    server updates with sequelize setup

--- server/app.ts
+++ server/app.ts
@@ -1,6 +1,18 @@
 import express, { Application, Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
+const sequelize = require('./models').sequelize;
+
 const app: Application = express();
+
+sequelize
+  .authenticate()
+  .then(() => {
+    if (process.env.NODE_ENV !== 'test')
+      console.log('Database connection has been established successfully.');
+  })
+  .catch((error: string) =>
+    console.error('Unable to connect to the database:', error)
+  );
 
 app.get('/', (req: Request, res: Response) => {
   res.status(StatusCodes.OK).json({ message: 'Palawatta Warriers Server' });
