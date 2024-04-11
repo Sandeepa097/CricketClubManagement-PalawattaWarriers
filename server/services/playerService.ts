9c5feac52d50cf3b192f41d7c8c46720dbd1a792
Gihan Sandeepa <gihan4880@gmail.com>
2024-04-11 23:45:26 +0530

    best batting and bowling score retrive

--- server/services/playerService.ts
+++ server/services/playerService.ts
@@ -1,6 +1,12 @@
 import { Op } from 'sequelize';
 import { PlayerMainRolls } from '../constants/PlayerMainRolls';
-import { Player } from '../models';
+import {
+  Match,
+  MatchPlayerBattingStat,
+  MatchPlayerBowlingStat,
+  Player,
+} from '../models';
+import sequelizeConnection from '../config/sequelizeConnection';
 
 interface CreatePlayerAttributes {
   avatar?: string | null;
@@ -39,4 +45,67 @@ export const removePlayer = async (id: number) => {
 
 export const getPlayers = async () => {
   return await Player.findAll();
+};
+
+export const getPlayerBattingStats = async (
+  playerId: number | string,
+  matchType: 'ppl' | 'outdoor'
+) => {
+  const bestScore = await MatchPlayerBattingStat.findOne({
+    where: { playerId },
+    attributes: [
+      'playerId',
+      'balls',
+      'isOut',
+      [
+        sequelizeConnection.fn('MAX', sequelizeConnection.col('score')),
+        'score',
+      ],
+    ],
+    group: ['playerId', 'balls', 'isOut'],
+    include: [
+      {
+        model: Match,
+        as: 'match',
+        attributes: ['id', 'oppositeTeamId'],
+        where: {
+          oppositeTeamId: { [matchType === 'ppl' ? Op.is : Op.not]: null },
+        },
+      },
+    ],
+    order: sequelizeConnection.literal('score DESC'),
+  });
+
+  return { bestScore };
+};
+
+export const getPlayerBowlingStats = async (
+  playerId: number | string,
+  matchType: 'ppl' | 'outdoor'
+) => {
+  const bestScore = await MatchPlayerBowlingStat.findOne({
+    where: { playerId },
+    attributes: [
+      'playerId',
+      'overs',
+      [
+        sequelizeConnection.fn('MAX', sequelizeConnection.col('wickets')),
+        'wickets',
+      ],
+    ],
+    group: ['playerId', 'overs'],
+    include: [
+      {
+        model: Match,
+        as: 'match',
+        attributes: ['id', 'oppositeTeamId', 'numberOfDeliveriesPerOver'],
+        where: {
+          oppositeTeamId: { [matchType === 'ppl' ? Op.is : Op.not]: null },
+        },
+      },
+    ],
+    order: sequelizeConnection.literal('wickets DESC'),
+  });
+
+  return { bestScore };
 };
