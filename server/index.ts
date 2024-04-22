9217c9650731645d86ffffe041d4fdfea2c2ca91
Gihan Sandeepa <gihan4880@gmail.com>
2024-04-22 22:20:15 +0530

    server clustered

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,19 @@
+import cluster from 'cluster';
+import os from 'os';
 import app from './app';
 import { PORT } from './config/config';
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+if (cluster.isPrimary) {
+  const numberOfWorkers = os.cpus().length;
+  for (var i = 0; i < numberOfWorkers; i++) {
+    cluster.fork();
+  }
+
+  cluster.on('exit', function () {
+    cluster.fork();
+  });
+} else {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
