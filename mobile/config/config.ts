afb7861de3da019abcf04f4dd00e76e441e37cc3
Gihan Sandeepa <gihan4880@gmail.com>
2024-05-05 15:22:32 +0530

    default envs

--- mobile/config/config.ts
+++ mobile/config/config.ts
@@ -1,4 +1,5 @@
-const API_URL = process.env.EXPO_PUBLIC_API_URL;
-const PAGE_SIZE = Number(process.env.EXPO_PUBLIC_PAGE_SIZE);
+const API_URL =
+  process.env.EXPO_PUBLIC_API_URL || 'https://vps25.heliohost.us/api';
+const PAGE_SIZE = Number(process.env.EXPO_PUBLIC_PAGE_SIZE || 25);
 
 export { API_URL, PAGE_SIZE };
