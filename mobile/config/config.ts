1a782e3c2f2c8c9e5b599cb86a5e736b4b69e8eb
Gihan Sandeepa <gihan4880@gmail.com>
2024-05-05 14:54:31 +0530

    default page size

--- mobile/config/config.ts
+++ mobile/config/config.ts
@@ -1,4 +1,4 @@
 const API_URL = process.env.EXPO_PUBLIC_API_URL;
-const PAGE_SIZE = Number(process.env.EXPO_PUBLIC_PAGE_SIZE);
+const PAGE_SIZE = Number(process.env.EXPO_PUBLIC_PAGE_SIZE || 25);
 
 export { API_URL, PAGE_SIZE };
